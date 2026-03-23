0ad367c304a511e3e22d0770d737a54f3a092d82
San0575 <s.nunezg@yahoo.com>
2026-03-23 17:10:48 +1100

    modi5

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -12,16 +12,14 @@
 	  name                = var.vnet_name
 	  address_space       = ["10.0.0.0/16"]
 	  location            = var.location
-	  resource_group_name = var.resource_group_name
+	  resource_group_name = azurerm_resource_group.rg.name
 	  depends_on = [azurerm_resource_group.rg]
 	}
 	
 	resource "azurerm_subnet" "subnet" {
 	  name                 = var.subnet_name
-	  resource_group_name  = var.resource_group_name
-	  virtual_network_name = var.vnet_name
+	  resource_group_name  = azurerm_resource_group.rg.name
+	  virtual_network_name = azurerm_virtual_network.vnet.name
 	  address_prefixes     = ["10.0.1.0/24"]
-	  depends_on = [azurerm_virtual_network.vnet, azurerm_resource_group.rg]
-	  virtual_network_name = var.vnet_name
-	  address_prefixes     = ["10.0.1.0/24"]
-	}
+	  depends_on = [azurerm_virtual_network.vnet]
+	 }
