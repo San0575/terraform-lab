11240019cffec6834e22aee1f6d07565bf60b925
San0575 <s.nunezg@yahoo.com>
2026-03-31 15:29:44 +1100

    Update AKS configuration in main.tf

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -3,18 +3,20 @@ resource "azurerm_kubernetes_cluster" "aks" {
   location            = var.location
   resource_group_name = var.resource_group_name
   dns_prefix          = "aks-${var.env}"
-  kubernetes_version  = "null"
+  kubernetes_version  = "1.27.0"
 
   default_node_pool {
-    name       = "default"
-    node_count = 1
-    vm_size    = "Standard_D2s_v3"
+    name           = "default"
+    node_count     = 1
+    vm_size        = "Standard_D2s_v3"
     vnet_subnet_id = var.subnet_id
   }
 
   identity {
     type = "SystemAssigned"
   }
+
+  role_based_access_control_enabled = true
 
   tags = {
     environment = var.env
