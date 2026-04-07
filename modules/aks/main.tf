7c05db8c653b7263b342c174f5229766865ebed0
San0575 <s.nunezg@yahoo.com>
2026-04-07 14:02:39 +1000

    add AKS deployment to pipeline

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -3,13 +3,19 @@ resource "azurerm_kubernetes_cluster" "aks" {
   location            = var.location
   resource_group_name = var.resource_group_name
   dns_prefix          = "aks-${var.env}"
-  kubernetes_version  = "1.27.0"
+  kubernetes_version  = "1.32.11"
 
   default_node_pool {
     name           = "default"
     node_count     = 1
     vm_size        = "Standard_D2s_v3"
     vnet_subnet_id = var.subnet_id
+  }
+
+  network_profile {
+    network_plugin = "azure"
+    service_cidr   = "10.1.0.0/16"
+    dns_service_ip = "10.1.0.10"
   }
 
   identity {
